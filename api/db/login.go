8c744125ca2b7551621f2fd8437be8f1e50b860a
RyhoBtw <114360068+RyhoBtw@users.noreply.github.com>
2023-03-08 03:13:04 -0800

    .

--- api/db/login.go
+++ api/db/login.go
@@ -1,1 +1,5 @@
 package db
+
+func Login(w http.ResponsWriter, r *http.Request) {
+	w.
+}
--- api/handleRequests.go
+++ api/handleRequests.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/RyhoBtw/3D-printer-api/api/printer"
+	"github.com/RyhoBtw/3D-printer-api/api/db"
 	"github.com/gorilla/mux"
 )
 
@@ -14,6 +15,7 @@ func handleRequests() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/print", HomePage)
 	router.HandleFunc("/print/status", printer.GetStatus)
+	router.HandleFunc("/print/login{info}", db.login)
 	router.HandleFunc("/print/Gcode", printer.PostGcode).Methods("POST")
 	log.Log().Fatal(http.ListenAndServe(":8000", router))
 }
